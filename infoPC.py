02798b030e8c45c37bdad3113f5cc732d3dfe96d
Daniil_0712q1 <daniillutaev1@gmail.com>
2023-02-15 09:52:03 +0400

    ыва

--- infoPC.py
+++ infoPC.py
@@ -1,9 +1,11 @@
 import platform
 import psutil
-import cpuinfo
 from cpuinfo import get_cpu_info
+import GPUtil
+import wmi
 
 def information():
+    print('Имя пользователя компьютера: ' + psutil.users()[0][0]) #Получаем имя пользователя компьютера
     print('На вашем компьютере установлена: ' + platform.system()) #Получаем название системы
     print('Полное название системы: ' + platform.platform()) #Получаем полное название системы
     print('Версия вашей системы: ' + platform.version()) #Получаем версию системы
@@ -14,11 +16,13 @@ def information():
     print('Процессор имеет физических ядер: ' + str(psutil.cpu_count(logical=False))) #Физ. ядра процессора
     print('Процессор имеет логических ядер: ' + str(psutil.cpu_count())) #Лог. ядра процессора
     print('\n')
-    print('Машина имеет оперативную память: ' + str(round((psutil.virtual_memory()[0])/1073741824)) + ' GB')
+    print(wmi.WMI().Win32_BaseBoard()[0].Product) #Получаем название материнской платы
+    print('Компьютер имеет оперативную память: ' + str(round((psutil.virtual_memory()[0])/1073741824)) + ' GB') #Получаем весь объем оперативной памяти ПК
     print('\n')
     print('Главный диск C имеет: ' + str(round(psutil.disk_usage('/')[0]/1073741824)) + ' GB')
-    print(psutil.disk_io_counters(perdisk=False)[4]) #время чтения с диска, миллисекунды
-    print(psutil.disk_io_counters(perdisk=False)[5]) #время записи на диск, миллисекунды
+    print('\n')
+    print(GPUtil.getAvailable())
+    
     
     
 if __name__ == '__main__':
