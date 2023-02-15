1a316a67ce3e78eaeaba2de7a5aa637f1619e988
Daniil_0712q1 <daniillutaev1@gmail.com>
2023-02-15 08:31:24 +0400

    fix

--- infoPC.py
+++ infoPC.py
@@ -7,8 +7,15 @@ def information():
     print('На вашем компьютере установлена: ' + platform.system()) #Получаем название системы
     print('Полное название системы: ' + platform.platform()) #Получаем полное название системы
     print('Версия вашей системы: ' + platform.version()) #Получаем версию системы
-    print(get_cpu_info())
+    # print(get_cpu_info())
+    print('\n')
     print('Бренд процессора компьютера: ' + get_cpu_info()['brand_raw'])
+    print('Такатовая частота процессора: ' + get_cpu_info()['hz_actual_friendly'])
+    print('Процессор имеет физических ядер: ' + str(psutil.cpu_count(logical=False)))
+    print('Процессор имеет логических ядер: ' + str(psutil.cpu_count()))
+    print('\n')
+    print('Машина имеет оперативную память: ' + str(round((psutil.virtual_memory()[0])/1073741824)) + 'GB')
+
     
     
 if __name__ == '__main__':
