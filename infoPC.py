040a3cc0050c050a76355e0d29fc5626097bd00d
Daniil_0712q1 <daniillutaev1@gmail.com>
2023-02-15 08:45:54 +0400

    sdf

--- infoPC.py
+++ infoPC.py
@@ -9,13 +9,16 @@ def information():
     print('Версия вашей системы: ' + platform.version()) #Получаем версию системы
     # print(get_cpu_info())
     print('\n')
-    print('Бренд процессора компьютера: ' + get_cpu_info()['brand_raw'])
-    print('Такатовая частота процессора: ' + get_cpu_info()['hz_actual_friendly'])
-    print('Процессор имеет физических ядер: ' + str(psutil.cpu_count(logical=False)))
-    print('Процессор имеет логических ядер: ' + str(psutil.cpu_count()))
+    print('Бренд процессора компьютера: ' + get_cpu_info()['brand_raw']) #Название процессора
+    print('Такатовая частота процессора: ' + get_cpu_info()['hz_actual_friendly']) #Тактовая частота процессора
+    print('Процессор имеет физических ядер: ' + str(psutil.cpu_count(logical=False))) #Физ. ядра процессора
+    print('Процессор имеет логических ядер: ' + str(psutil.cpu_count())) #Лог. ядра процессора
     print('\n')
-    print('Машина имеет оперативную память: ' + str(round((psutil.virtual_memory()[0])/1073741824)) + 'GB')
-
+    print('Машина имеет оперативную память: ' + str(round((psutil.virtual_memory()[0])/1073741824)) + ' GB')
+    print('\n')
+    print('Главный диск C имеет: ' + str(round(psutil.disk_usage('/')[0]/1073741824)) + ' GB')
+    print(psutil.disk_io_counters(perdisk=False)[4]) #время чтения с диска, миллисекунды
+    print(psutil.disk_io_counters(perdisk=False)[5]) #время записи на диск, миллисекунды
     
     
 if __name__ == '__main__':
